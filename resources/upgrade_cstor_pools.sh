fe17c0611d462d8ecd83e802fd58ac3666af4d9e
Lenko <lenko@gravitational.com>
2020-11-02 16:04:05 -0600

    Added checks for missing volumes.

--- resources/upgrade_cstor_pools.sh
+++ resources/upgrade_cstor_pools.sh
@@ -4,7 +4,19 @@
       while read line; do echo "${1}${line}"; done;
   }
 
-kubectl get spc | cut -d" " -f1 | tail -n +2 >cstor_pools.txt
+#set -v -x -e
+
+kubectl get spc -A | cut -d" " -f1 | tail -n +2 >cstor_pools.txt
+if [ -s cstor_pools.txt ]
+then
+    echo "found cStor pools:"
+    cat cstor_pools.txt
+else
+  echo "Unable to find any cStor Pools."
+  exit 0
+fi
+
+
 sed 's/[^[:space:],]\+/"&"/g' cstor_pools.txt > cstor_pools2.txt
 cat cstor_pools2.txt | prepend "- " > cstor_pools3.txt
 
@@ -18,3 +30,5 @@ cStorPools=$(<cstor_pools3.txt)
   while IFS= read -r line; do
     echo "${line//$cStorPoolsStrToReplace/$cStorPools}"
   done < /var/lib/gravity/resources/upgrade_cstor_pools.yaml
+
+#set +v +x +e
